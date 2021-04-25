619574fdf942174dfab943610001e901d8b687d8
Juan Manuel Gómez Galeano <juanmgga@ext.zara.com>
2021-04-25 21:53:18 +0200

    [Dog]: added defending and running animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 	public Rigidbody rb;
 	float speed = 4;	
-	float rotSpeed = 80;
+	float rotSpeed = 120;
 	float rot = 0;
 	float gravity = 8;
 
@@ -32,33 +32,76 @@ public class PlayerMovement : MonoBehaviour
 
 	void Movement()
 	{
-		if(controller.isGrounded)
+		if (anim.GetBool("attacking") || anim.GetBool("defending"))
 		{
-			if (Input.GetKey(KeyCode.W))
-			{
-				if(anim.GetBool("attacking") == true)
-				{
-					return;
-				}
-				else if(anim.GetBool("attacking") == false)
-				{
-				anim.SetBool("walking", true);
-				anim.SetInteger("condition", 1);
-				moveDir = new Vector3(0,0,1);
-				moveDir *= speed;
-				moveDir = transform.TransformDirection(moveDir);
-				}
-			} 
-			if (Input.GetKeyUp(KeyCode.W))
-			{
-				anim.SetBool("walking", false);
-				anim.SetInteger("condition", 0);
-				moveDir = new Vector3(0,0,0);
-			} 
+			// Don't move while attacking
+			return;
 		}
+		else
+		{
+			if(controller.isGrounded)
+			{
+				// Moving forward - pressing W
+				if (Input.GetKey(KeyCode.W))
+				{
+					anim.SetBool("walking", true);
+					anim.SetInteger("condition", 1); // walking animation
+
+					moveDir = new Vector3(0,0,1);
+					moveDir *= speed;
+					
+					// Running - pressing left shift
+					if (Input.GetKey(KeyCode.LeftShift))
+					{
+						anim.SetBool("walking", false);
+						anim.SetBool("running", true);
+						anim.SetInteger("condition", 4); // running animation
+						moveDir *= 1.5f;
+					}
+
+					moveDir = transform.TransformDirection(moveDir);
+					
+				}
+
+				// Moving backwards - pressing S
+				if (Input.GetKey(KeyCode.S))
+				{
+					anim.SetBool("walking", true);
+					anim.SetInteger("condition", 1); // walking animation
+
+					moveDir = new Vector3(0,0,-1);
+					moveDir *= speed;
+					moveDir = transform.TransformDirection(moveDir);
+					
+				}
+
+				// Jumping - pressing spacebar
+				if (Input.GetKey(KeyCode.Space))
+				{
+					anim.SetBool("walking", false);
+					anim.SetInteger("condition", 0);
+
+					moveDir = new Vector3(0, 1, 0);
+					moveDir *= speed;
+					moveDir = transform.TransformDirection(moveDir);
+				}
+
+				// Stopping - releasing
+				if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+				{
+					anim.SetBool("walking", false);
+					anim.SetInteger("condition", 0); // Idle animation
+
+					moveDir = new Vector3(0,0,0);
+				} 
+			}
+		}
+
+		// Apply rotation
 		rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
 		transform.eulerAngles = new Vector3(0,rot,0);
 
+		// Apply gravity
 		moveDir.y -= gravity * Time.deltaTime;
 		controller.Move(moveDir * Time.deltaTime);
 	}
@@ -67,16 +110,32 @@ public class PlayerMovement : MonoBehaviour
 	{
 		if(controller.isGrounded)
 		{
+			// If left mouse is pressed, it'll attack as long as it's not walking
 			if (Input.GetMouseButtonDown(0))
 			{	
-				if(anim.GetBool("walking") == true)
+				if(anim.GetBool("walking") || anim.GetBool("running"))
 				{
 					anim.SetBool("walking", false);
-					anim.SetInteger("condition", 0);
+					anim.SetBool("running", false);
+					anim.SetInteger("condition", 0); // Idle animation
 				}
-				if (anim.GetBool("walking") == false)
+				if (!anim.GetBool("walking") && !anim.GetBool("running"))
 				{
 					Attack();
+				}
+			}
+			// If right mouse is pressed, it'll defend as long as it's not walking
+			if (Input.GetMouseButtonDown(1))
+			{	
+				if(anim.GetBool("walking") || anim.GetBool("running"))
+				{
+					anim.SetBool("walking", false);
+					anim.SetBool("running", false);
+					anim.SetInteger("condition", 0); // Idle animation
+				}
+				if (!anim.GetBool("walking") && !anim.GetBool("running"))
+				{
+					Defend();
 				}
 			}
 		}
@@ -90,9 +149,27 @@ public class PlayerMovement : MonoBehaviour
 	IEnumerator AttackRoutine()
 	{
 		anim.SetBool("attacking", true);
-		anim.SetInteger("condition", 2);
-		yield return new WaitForSeconds(1);
+		anim.SetInteger("condition", 2); // Attacking animation
+
+		yield return new WaitForSeconds(0.9f);
+
 		anim.SetInteger("condition", 0);
 		anim.SetBool("attacking", false);
+	}
+
+	void Defend()
+	{
+		StartCoroutine(DefendRoutine());
+	}
+
+	IEnumerator DefendRoutine()
+	{
+		anim.SetBool("defending", true);
+		anim.SetInteger("condition", 3); // Defending animation
+
+		yield return new WaitForSeconds(0.9f);
+
+		anim.SetInteger("condition", 0);
+		anim.SetBool("defending", false);
 	}
 }
