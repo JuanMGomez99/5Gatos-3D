fe8f8e2f23435c630412a3e48a40a2cadd83a3c7
nonari <alejandro.budino@gmail.com>
2021-05-12 19:26:32 +0200

    PowerUp

---
+++ Assets/PowerUp/CollisionNotify.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionNotify : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider collider) {
+        Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.name == "DogPolyart") {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/PowerUp/Effect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(75 * Vector3.right);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(10 * Vector3.forward * Time.deltaTime);
+        
+    }
+}
