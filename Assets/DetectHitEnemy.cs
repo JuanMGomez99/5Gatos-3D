29aafc0d2d5909846cf50edff7eaf43e6945b7f9
Juan Manuel Gómez Galeano <juanmgga@ext.zara.com>
2021-05-17 18:13:36 +0200

    [DetectHitEnemy] cuando muere NPC, las colisiones no aplican

--- Assets/DetectHitEnemy.cs
+++ Assets/DetectHitEnemy.cs
@@ -8,19 +8,25 @@ public class DetectHitEnemy : MonoBehaviour
 	public Slider healthBar;
 	public Transform player;
 	Animator anim;
+	public Rigidbody rb;
 
 	void OnTriggerEnter(Collider other)
 	{
 		healthBar.value -= 10;
 
 		if(healthBar.value <= 0)
+		{
 			anim.SetBool("is_dead", true);
+			rb.isKinematic = true;
+			rb.detectCollisions = false;
+		}
 	}
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
